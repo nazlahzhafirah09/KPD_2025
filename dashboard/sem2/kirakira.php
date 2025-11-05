5eef06bc0ac65814cf0694e15f4c52eccd947cbd
nazlahzhafirah09 <zhafirahridzuan09@gmail.com>
2025-11-05 15:53:26 +0800

    First Commit

---
+++ dashboard/sem2/kirakira.php
@@ -0,0 +1,25 @@
+<?php
+//integer
+$num1 = 12;
+$num2 = 6;
+//float
+$num3 = 5.3;
+
+//operasi dua nombor
+echo "Hasil tambah $num1 + $num2 =";
+echo $num1 + $num2;
+echo "<br>";
+
+echo "Hasil tambah $num1 - $num2 =";
+echo $num1 - $num2;
+echo "<br>";
+
+echo "Hasil tambah $num1 * $num2 =";
+echo $num1 * $num2;
+echo "<br>";
+
+echo "Hasil tambah $num1 / $num3 =";
+echo $num1 / $num3;
+echo "<br>";
+
+?>
---
+++ dashboard/sem2/phpinfo.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+$nama = "Ali";
+$umur = 25;
+$berat = 25.5;
+
+echo "Nama :" .$nama. "<br>"; 
+echo "Nama :" .$umur. "<br>";
+echo "Nama :" .$berat. "<br>";
+
+?>
+
+
